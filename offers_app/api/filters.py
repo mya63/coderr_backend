edb927a34a132d3ffc3bab938d9e8998c26dd2a5
mya63 <muhammedyunusamini4@gmail.com>
2026-03-05 10:26:51 +0100

    Fix offer min_price filter to use gte instead of lte

--- offers_app/api/filters.py
+++ offers_app/api/filters.py
@@ -7,20 +7,22 @@ from offers_app.models import Offer
 
 
 class OfferFilter(df.FilterSet):
-    creator_id = df.NumberFilter(field_name="user_id")  # MYA
+    creator_id = df.NumberFilter(field_name="user_id")
 
-    # MYA: Filter auf annotierte Werte
+    # filter based on annotated fields
     min_price = df.NumberFilter(method="filter_min_price")
     max_delivery_time = df.NumberFilter(method="filter_max_delivery_time")
 
     def filter_min_price(self, queryset, name, value):
         if value is None:
             return queryset
-        return queryset.filter(min_price__lte=value)
+        # return offers where minimum price is >= given value
+        return queryset.filter(min_price__gte=value)
 
     def filter_max_delivery_time(self, queryset, name, value):
         if value is None:
             return queryset
+        # return offers where delivery time is <= given value
         return queryset.filter(min_delivery_time__lte=value)
 
     class Meta:
